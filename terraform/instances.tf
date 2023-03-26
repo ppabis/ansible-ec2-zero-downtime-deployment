3a0c7f544bb42fa7a602155c33065a09bef23a6f
ppabis <piotr@pabis.eu>
2023-03-26 20:08:40 +0200

    Found a matching ami

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -4,9 +4,32 @@ data "aws_ami" "amzn2" {
   owners = ["amazon"]
 
   filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-arm64-ebs"]
+    name   = "root-device-type"
+    values = ["ebs"]
   }
+
+  filter {
+    name = "architecture"
+    values = ["arm64"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+}
+
+output "ami-name" {
+  value = data.aws_ami.amzn2.name 
+}
+
+output "ami-descr" {
+  value = data.aws_ami.amzn2.description
 }
 
 resource "aws_instance" "app" {
