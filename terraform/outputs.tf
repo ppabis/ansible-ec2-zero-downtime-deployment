abd720f919e6f13de5b2584ddbaee642815bbe0d
ppabis <piotr@pabis.eu>
2023-03-27 19:52:34 +0200

    add target group arn output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,10 @@ output "ami-descr" {
   value = data.aws_ami.amzn2.description
 }
 
-/* output "ips" {
+output "ips" {
   value = aws_instance.app.*.public_ip
-} */
+}
+
+output "target_group_arn" {
+    value = aws_alb_target_group.apps.arn
+}
