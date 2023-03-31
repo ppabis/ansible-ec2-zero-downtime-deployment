0192607b7a4f0e1e9f499cfd8e54be88a9223d8e
ppabis <piotr@pabis.eu>
2023-03-31 20:16:26 +0200

    Added versioning to the service

--- ansible/service.py
+++ ansible/service.py
@@ -3,6 +3,15 @@ from socket import gethostname
 from hashlib import sha256
 from datetime import datetime
 from timeit import default_timer as timer
+from os.path import exists
+
+VERSION = "?Unknown?"
+
+def get_version():
+    global VERSION
+    if exists('/opt/version'):
+        with open('/opt/version', 'r') as f:
+            VERSION = f.read()
 
 def lotsofcrypto(iters):
     """
@@ -17,11 +26,14 @@ def lotsofcrypto(iters):
 
 def generate_response(iters=250000):
     myhash, timed = lotsofcrypto(iters)
-    return f""" This is {gethostname()}.
-            My hash is {myhash} and took me {timed}s for {iters} iterations.
+    return f"""
+    Version: {VERSION}
+    This is {gethostname()}.
+    My hash is {myhash} and took me {timed}s for {iters} iterations.
     """.encode()
 
 class MyHandler(BaseHTTPRequestHandler):
+
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
@@ -31,8 +43,10 @@ class MyHandler(BaseHTTPRequestHandler):
         else:
             self.wfile.write(generate_response())
         return
-    
+
 def run():
+    get_version()
+    print(f"VERSION: {VERSION}")
     print('starting server...')
     server_address = ('0.0.0.0', 80)
     httpd = HTTPServer(server_address, MyHandler)
