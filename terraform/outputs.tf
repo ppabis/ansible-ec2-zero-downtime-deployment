c51ac775bd6b96e6d1c702382b7205a4c8055de5
ppabis <piotr@pabis.eu>
2023-03-31 22:21:38 +0200

    Revert "remove costly things' outputs"
    This reverts commit 9b1ed94b790b3261ebb8fef741799e2c2ed8e265.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,14 +6,14 @@ output "ami-descr" {
   value = data.aws_ami.amzn2.description
 }
 
-/* output "ips" {
+output "ips" {
   value = aws_instance.app.*.public_ip
-} */
+}
 
 output "target_group_arn" {
   value = aws_alb_target_group.apps.arn
 }
 
-/* output "elb-cname" {
+output "elb-cname" {
   value = aws_lb.loadbalancer.dns_name
-} */
+}
